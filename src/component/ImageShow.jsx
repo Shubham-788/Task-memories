f5601ec2de2d8a9d24940a026f250d56e2174c9e
Shubham-788 <21cs01030@iitbbs.ac.in>
2024-04-24 19:25:48 +0530

    small changes

--- src/component/ImageShow.jsx
+++ src/component/ImageShow.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import { useSpring, animated } from "react-spring";
-import ProgressBar from "./ProgressBar";
 
 const ImageShow = ({ imageData, currentImageIndex, handleClick, time }) => {
   // Define the spring animation for zoom effect
